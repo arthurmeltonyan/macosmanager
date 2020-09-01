731d201d8a89f8608388c2dcc0c902a429ab39c1
Arthur Meltonyan <42479733+arthurmeltonyan@users.noreply.github.com>
2020-09-01 18:12:22 +0300

    Update install_python_libraries.fish

--- install_python_libraries.fish
+++ install_python_libraries.fish
@@ -80,7 +80,7 @@ poetry add scipy numpy numba swifter pandas xlrd xlsxwriter
 poetry add matplotlib seaborn cufflinks chart_studio plotly
 poetry add streamlit fastapi uvicorn
 poetry add dephell -E complete
-poetry add mypy typing_extensions marshmallow
+poetry add mypy typing_extensions marshmallow attr
 poetry add peewee pony
 poetry add pytest pyinstrument
 poetry add jupyterlab ipywidgets tqdm
