e5518f24869a48b7dcbec77f810749e109c6d9d4
Arthur Meltonyan <42479733+arthurmeltonyan@users.noreply.github.com>
2021-07-11 13:02:37 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 
 
-curl -O https://raw.githubusercontent.com/arthurmeltonyan/macosmanager/master/install_macos_applications.sh
-osascript -e 'tell app "Terminal" to do script "source install_macos_applications.sh"'
+curl -O https://raw.githubusercontent.com/arthurmeltonyan/macosmanager/master/install_applications.sh
+osascript -e 'tell app "Terminal" to do script "source install_applications.sh"'
