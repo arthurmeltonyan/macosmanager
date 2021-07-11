58d57794874659523e495cc0492762e963d19293
Arthur Meltonyan <42479733+arthurmeltonyan@users.noreply.github.com>
2021-07-11 17:26:19 +0300

    Delete install.sh

--- install.sh
+++
@@ -1,5 +0,0 @@
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-
-
-curl -O https://raw.githubusercontent.com/arthurmeltonyan/macosmanager/master/install_applications.sh
-osascript -e 'tell app "Terminal" to do script "source install_applications.sh"'
