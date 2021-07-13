a7f8ec6763afbdc11c665308ba0fc0827616ac06
Arthur Meltonyan <42479733+arthurmeltonyan@users.noreply.github.com>
2021-07-13 11:56:03 +0300

    Update install.zsh

--- install.zsh
+++ install.zsh
@@ -1,9 +1,9 @@
 touch ~/.zshrc
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-brew install starship
-echo "eval "$(starship init zsh)"" >> ~/.zshrc
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+brew install starship
+echo "eval "$(starship init zsh)"" >> ~/.zshrc
 brew tap homebrew/cask-fonts
 brew install --cask font-fira-code-nerd-font
 touch ~/.hushlogin
